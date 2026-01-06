a8b7bdba760761475c567792360cb68749afceba
IGS (Roscan Teodor) <teodorroscan@gmail.com>
2026-01-06 20:09:55 +0200

    File Hasher 1.0.0 (Setup)

---
+++ GenericPlugins/FileHasher/include/FileHasher.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "GView.hpp"
+
+namespace GView::GenericPlugins::FileHasher
+{
+} // namespace GView::GenericPlugins::FileHasher
---
+++ GenericPlugins/FileHasher/src/FileHasher.cpp
@@ -0,0 +1,45 @@
+#include "FileHasher.hpp"
+
+using namespace AppCUI;
+using namespace AppCUI::Utils;
+using namespace AppCUI::Application;
+using namespace AppCUI::Controls;
+using namespace GView::Utils;
+using namespace GView;
+using namespace GView::View;
+
+constexpr int CMD_BUTTON_CLOSE = 1;
+
+class FileHasher : public Window, public Handlers::OnButtonPressedInterface
+{
+  public:
+    FileHasher() : Window("Character Table", "d:c,w:70,h:20", WindowFlags::Sizeable | WindowFlags::Maximized)
+    {
+        Factory::CharacterTable::Create(this, "l:1,t:1,r:1,b:3");
+        Factory::Button::Create(this, "&Close", "d:b,w:20", CMD_BUTTON_CLOSE)->Handlers()->OnButtonPressed = this;
+    }
+    void OnButtonPressed(Reference<Button>) override
+    {
+        this->Exit();
+    }
+};
+
+extern "C"
+{
+    PLUGIN_EXPORT bool Run(const string_view command, Reference<GView::Object> currentObject)
+    {
+        // all good
+        if (command == "FileHasher")
+        {
+            FileHasher dlg;
+            dlg.Show();
+            return true;
+        }
+        return false;
+    }
+
+    PLUGIN_EXPORT void UpdateSettings(IniSection sect)
+    {
+        sect["Command.FileHasher"] = Input::Key::Ctrl | Input::Key::Alt | Input::Key::Shift | Input::Key::F10;
+    }
+}
